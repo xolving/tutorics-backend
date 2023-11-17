08bea8573702ee6225f293f98f6efad0ad0481f9
6283hz <lead@xolving.com>
2023-11-17 15:54:32 +0900

    initial project

---
+++ src/main/kotlin/com/xolving/tutorics/TutoricsApplication.kt
@@ -0,0 +1,11 @@
+package com.xolving.tutorics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutoricsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TutoricsApplication>(*args)
+}
---
+++ src/test/kotlin/com/xolving/tutorics/TutoricsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xolving.tutorics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutoricsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
